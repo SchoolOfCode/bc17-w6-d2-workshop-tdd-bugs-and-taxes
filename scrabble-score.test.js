0e1a64807ca3754ff5ccdcc4a2749d1cadecf19c
Richard Lamb <lamby42@live.co.uk>
2024-08-06 13:29:37 +0100

    added test.each for every letter. now to add those letter scores to the function as it only passes a

--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -4,3 +4,47 @@ import { expect, test } from "vitest";
 test("bare bones test", () => {
     expect(calculateScrabbleScore("A")).toBe(1);
 });
+
+test.each([
+    ["a", 1],
+    ["b", 3],
+    ["c", 3],
+    ["d", 2],
+    ["e", 1],
+    ["f", 4],
+    ["g", 2],
+    ["h", 4],
+    ["i", 1],
+    ["j", 8],
+    ["k", 5],
+    ["l", 1],
+    ["m", 3],
+    ["n", 1],
+    ["o", 1],
+    ["p", 3],
+    ["q", 10],
+    ["r", 1],
+    ["s", 1],
+    ["t", 1],
+    ["u", 1],
+    ["v", 4],
+    ["w", 4],
+    ["x", 8],
+    ["y", 4],
+    ["z", 10],
+])("%s -> %i", (a, expected) => {
+    expect(calculateScrabbleScore(a)).toBe(expected);
+});
+
+// test.each([
+//     [1, 1, 2],
+//     [1, 2, 3],
+//     [2, 1, 3],
+// ])("add(%i, %i) -> %i", (a, b, expected) => {
+//     expect(a + b).toBe(expected);
+// });
+
+// // this will return
+// // ✓ add(1, 1) -> 2
+// // ✓ add(1, 2) -> 3
+// // ✓ add(2, 1) -> 3
